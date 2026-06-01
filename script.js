8642b5b0a7ccb9d4cec2cf4ec6a1448160ad4385
afnanetr <afnanetrab@gmail.com>
2026-06-01 02:31:55 +0100

    Update character picking animation based on direction and extend picking time

--- script.js
+++ script.js
@@ -32,19 +32,26 @@ const spriteMap = {
     front: "character_s_front.png"
 };
 
+const pickSpriteMap = {
+    left: "character_pick_left.png",
+    right: "character_s_right.png",
+    back: "character_s_back.png",
+    front: "character_pick.png"
+};
+
 function collectLetter(letter){
 
     if(isPicking) return;
 
     isPicking = true;
 
-    player.src = "character_pick.png";
+    player.src = pickSpriteMap[lastDirection];
 
     setTimeout(() => {
         letter.remove(); // Remove the letter element completely
         isPicking = false;
         player.src = spriteMap[lastDirection];
-    }, 500);
+    }, 1500);
 }
 
 function checkLetters(){
